98ed2ca3ed711148ba5860000c7691a0ed75b36c
Archer-4 <njackson6@csub.edu>
2021-05-09 20:06:37 -0700

    Removed Action Bar/Taken Progress Screenshots

--- app/src/main/java/com/example/squeegeespree/MainActivity.kt
+++ app/src/main/java/com/example/squeegeespree/MainActivity.kt
@@ -9,15 +9,25 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val t = Toast.makeText(this@MainActivity, "Placeholder Button", Toast.LENGTH_SHORT)
 
         findViewById<Button>(R.id.playButton).setOnClickListener{
             startActivity(Intent(this@MainActivity, gamePlay::class.java))
+        }
+        findViewById<Button>(R.id.upGradeMenu).setOnClickListener {
+            //Needs implementation
+            t.show()
+        }
+        findViewById<Button>(R.id.statisticsMenu).setOnClickListener{
+            //Needs implementation
+            t.show()
         }
     }
 }
